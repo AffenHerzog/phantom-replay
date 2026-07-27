76bb11d1c7c9692c9ee6120b0dc199c5f3c15f52
jrothgaenger <julius.rothgaenger@gmail.com>
2026-07-27 22:02:51 +0200

    initial commit

---
+++ src/main/java/de/affenherzog/phantomReplay/PhantomReplay.java
@@ -0,0 +1,16 @@
+package de.affenherzog.phantomReplay;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PhantomReplay extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
